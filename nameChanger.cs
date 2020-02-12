2c15eb8aede48a98f4ece6e6fc0e5925f5120813
Minh To <Minh.To@cantire.com>
2020-02-12 16:36:41 -0500

    added properties form, which extends from component types in Abacus

--- nameChanger.cs
+++ nameChanger.cs
@@ -1,7 +1,6 @@
 #ImportDLL Microsoft.VisualBasic.dll
 #ImportDLL System.Runtime.dll
 
-
 using System;
 using System.Linq;
 using System.Drawing;
@@ -14,6 +13,11 @@ using ABACUS.WPFUtil;
 
 namespace nameChangeSpace
 {
+	public static class WasChanged{
+		public static bool changed = false;
+	}
+	
+	
 	public class Form1 : Form
 	{
 		private ComboBox cb;
@@ -22,45 +26,41 @@ namespace nameChangeSpace
 		private ListBox lb1;
 		private CComponentType [] cTypes;
 		private Button changeBtn;
+		private CComponentType selectedComp;
 		
-		int[] testArr = new int[] {1,2,3,4};
-		
-		public Form1(){
+		public Form1()
+		{
 			this.InitializeComponent();
 		}
 
 		private void InitializeComponent()
 		{
-			this.cb = new ComboBox();
-			this.cb.Location = new Point(8, 248);
-			this.cb.Size = new Size(280, 21);
-			
+			this.Size = new Size(700, 400);
 			this.closeButton = new Button();
 			this.closeButton.Name = "Close";
-			this.closeButton.Text = "Close";
-			this.closeButton.Location = new Point(70, 220);
-			this.closeButton.Size = new Size(120, 20);
+			this.closeButton.Text = "Close (Trying to get this to work lol)";
+			this.closeButton.Location = new Point(280, 300);
+			this.closeButton.Size = new Size(120, 40);
 			this.closeButton.Click += new System.EventHandler(closeButton_Click);
+			
 			
 			this.changeBtn = new Button();
 			this.changeBtn.Text = "Change";
-			this.changeBtn.Location = new Point(70, 170);
+			this.changeBtn.Location = new Point(280, 260);
 			this.changeBtn.Size = new Size(120, 25);
 			this.changeBtn.Click += new System.EventHandler(changeBtn_Click);
 			
 			
 			this.lbLabel = new Label();
 			this.lbLabel.Text = "Select a component to modify";
-			this.lbLabel.Location = new Point(35, 40);
+			this.lbLabel.Location = new Point(200, 30);
 			this.lbLabel.Size = new Size(200, 20);
 			
 			this.lb1 = new ListBox();
-			this.lb1.Size = new Size(200, 100);
-			this.lb1.Location = new Point(35, 60);
-
-			this.cb.Items.Add(testArr);
-			this.Text = "Change Component Name";
+			this.lb1.Size = new Size(300, 200);
+			this.lb1.Location = new Point(180, 50);
 			
+			this.Text = "Change Component Name";
 			
 			this.Controls.AddRange(new Control[] {closeButton, lb1, lbLabel, changeBtn});
 			
@@ -77,26 +77,89 @@ namespace nameChangeSpace
 		}
 		public void changeBtn_Click(object sender, System.EventArgs e)
 		{
-			NameChanger nc = new NameChanger();
-			string newName;
 			foreach(CComponentType c in cTypes){
 				if(lb1.SelectedItem.ToString() == c.Name.ToString()){
-					nc.changeName(c);
+					selectedComp = c;
+					//send selected comp to form2
+					Form2 f2 = new Form2();
+					f2.loadProps(c);
 					break;
 				}
 			}
-	
+		}
+		
+		public CComponentType getSelectedComp()
+		{
+			return selectedComp;
 		}
 		 
-	
 		public void closeButton_Click(object sender, System.EventArgs e)
+		{
+			
+		}
+	}
+	
+	public class Form2 : Form{
+		//this form is to actually drill down into the properties
+		private ListBox propsBox = new ListBox();
+		private Button select = new Button();
+		private Label title = new Label();
+		private CComponentType comp;
+		private Button closeButton = new Button();
+		
+		public Form2()
+		{
+			InitializeComponent();
+		}
+		
+		private void InitializeComponent()
+		{
+			this.Size = new Size(700, 400);
+			this.propsBox.Size = new Size(300, 200);
+			this.propsBox.Location = new Point(180, 50);
+		
+			this.select.Text = "Change";
+			this.select.Location = new Point(280, 260);
+			this.select.Size = new Size(120, 25);
+			//this.select.Click += new System.EventHandler(changeBtn_Click);
+			
+			this.closeButton.Text = "Close";
+			this.closeButton.Location = new Point (280, 300);
+			this.closeButton.Size = new Size(120, 25);
+		
+			
+			this.title = new Label();
+			this.title.Text = "Select a property to modify";
+			this.title.Location = new Point(200, 30);
+			this.title.Size = new Size(200, 20);
+			
+			this.Controls.AddRange(new Control[] {title, closeButton, propsBox, select});
+		
+		}
+		public void loadProps(CComponentType c)
+		{
+			this.comp = c;
+			if(this.comp.Properties.Count > 0){
+				propsBox.BeginUpdate();
+				for(int i = 0; i < this.comp.Properties.Count; i++){
+					propsBox.Items.Add(this.comp.Properties[i]);
+				}
+				propsBox.EndUpdate();
+				this.Show();
+			}
+			else{
+				MessageBox.Show("This component has no properties!");
+			}
+		}
+		
+		private void changeBtn_Click()
 		{
 			
 		}
 	}
 
 	
-	public class NameChanger{
+	/*public class NameChanger{
 		
 		public NameChanger(){}
 
@@ -106,16 +169,17 @@ namespace nameChangeSpace
 			CComponentTypeCollection rootComps = objModel.ComponentTypes;
 			CComponentType[] allComps = rootComps.Clone();
 			
-			return allComps;
+			return allComps; //allComps just has the component types. each component type has properties. 
 		}
 				
 		public void changeName(CComponentType toChange)
 		{
-			
-			toChange.Name = Interaction.InputBox("Enter in the new component type name", "Change Name", toChange.ToString());
-	
-			
-			//message back how many fields changed.
+			string oldName = toChange.Name;
+			string newName = Interaction.InputBox("Enter in the new component type name", "Change Name", toChange.ToString());
+			toChange.Name = newName;
+			MessageBox.Show("Component type " + "\"" + oldName + "\"" + " changed to " + "\"" + newName + "\"");
+			WasChanged.changed = true;
+		
 		}
 			
 			
@@ -146,54 +210,19 @@ namespace nameChangeSpace
 		}
 			
 	
-	}
+	}*/
 	
   public class mainClass
 	{
 		public static void Main() 
 		{
 			CObjectModel objModel = ABACUSScriptingModule.ABACUSModel;
-		
-			//MessageBoxWPF.Show(objModel.ComponentTypes.TotalCount().ToString()); //returns 8
-			
-			//MessageBoxWPF.Show(rootComps.TotalCount().ToString());
+			CComponentTypeCollection rootComps = objModel.ComponentTypes;
+			CComponentType[] allComps = rootComps.Clone();
 			Form1 f1 = new Form1();
-			NameChanger nb = new NameChanger();
-			
-			f1.loadComboBox(nb.grabComps());
-			f1.Show();
-			
-			//else prompt for change
-			
-			//change
-			
-			
-			//confirm
-			
-			
-			/*foreach(CComponentType c in allComps){
-			MessageBoxWPF.Show(c.ToString());
-			}*/
-			//make input box that accepts a string
-			
-			//the allComps array contains all of the components in the metamodel 
-			//we need to get the service offering comp, and then change the property name.
-			
+			f1.loadComboBox(allComps);
+			f1.Show();		
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
